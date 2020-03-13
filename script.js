7b7951cf9f82a0b487f8efc675e4637fb2d1ba7c
catchupTJC <53647636+catchupTJC@users.noreply.github.com>
2020-03-14 02:18:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,74 @@
 onload = function() {
+  
+            if ('speechSynthesis' in window)
+                with (speechSynthesis) {
+
+                    var playEle = document.querySelector('#play');
+                    var pauseEle = document.querySelector('#pause');
+                    var stopEle = document.querySelector('#stop');
+                    var flag = false;
+
+                    playEle.addEventListener('click', onClickPlay);
+                    pauseEle.addEventListener('click', onClickPause);
+                    stopEle.addEventListener('click', onClickStop);
+
+                    function onClickPlay() {
+                        if (!flag) {
+                            flag = true;
+                            utterance = new SpeechSynthesisUtterance(document.querySelector('#readFrom').innerHTML);             
+
+                            let sval = Number(speakerMenu.value);                            
+                            utterance.voice = allVoices[sval];
+                            utterance.lang = utterance.voice.lang;
+                            utterance.rate = Number(rateFld.value);
+
+                            utterance.onend = function () {
+                                flag = false;
+                                playEle.className = pauseEle.className = '';
+                                stopEle.className = 'stopped';
+                            };
+                            playEle.className = 'played';
+                            stopEle.className = '';
+                            speak(utterance);
+                        }
+                        if (paused) {
+                            playEle.className = 'played';
+                            pauseEle.className = '';
+                            resume();
+                        }
+                    }
+
+                    function onClickPause() {
+                        if (speaking && !paused) {
+                            pauseEle.className = 'paused';
+                            playEle.className = '';
+                            pause();
+                        }
+                    }
+
+                    function onClickStop() {
+                        if (speaking) {
+                            stopEle.className = 'stopped';
+                            playEle.className = pauseEle.className = '';
+                            flag = false;
+                            cancel();
+
+                        }
+                    }
+
+                }
+
+            else { /* speech synthesis not supported */
+                msg = document.createElement('h5');
+                msg.textContent = "Detected no support for Speech Synthesis";
+                msg.style.textAlign = 'center';
+                msg.style.backgroundColor = 'red';
+                msg.style.color = 'white';
+                msg.style.marginTop = msg.style.marginBottom = 0;
+                document.body.insertBefore(msg, document.querySelector('div'));
+            }
+
+        }
 
   let allVoices;
   let rateFld, speakerMenu;
@@ -71,73 +141,3 @@ document.addEventListener('DOMContentLoaded', function (e) {
     console.log("Data didn't load", error);}
 
 }); 
-  
-            if ('speechSynthesis' in window)
-                with (speechSynthesis) {
-
-                    var playEle = document.querySelector('#play');
-                    var pauseEle = document.querySelector('#pause');
-                    var stopEle = document.querySelector('#stop');
-                    var flag = false;
-
-                    playEle.addEventListener('click', onClickPlay);
-                    pauseEle.addEventListener('click', onClickPause);
-                    stopEle.addEventListener('click', onClickStop);
-
-                    function onClickPlay() {
-                        if (!flag) {
-                            flag = true;
-                            utterance = new SpeechSynthesisUtterance(document.querySelector('#readFrom').innerHTML);             
-
-                            let sval = Number(speakerMenu.value);                            
-                            utterance.voice = allVoices[sval];
-                            utterance.lang = utterance.voice.lang;
-                            utterance.rate = Number(rateFld.value);
-
-                            utterance.onend = function () {
-                                flag = false;
-                                playEle.className = pauseEle.className = '';
-                                stopEle.className = 'stopped';
-                            };
-                            playEle.className = 'played';
-                            stopEle.className = '';
-                            speak(utterance);
-                        }
-                        if (paused) {
-                            playEle.className = 'played';
-                            pauseEle.className = '';
-                            resume();
-                        }
-                    }
-
-                    function onClickPause() {
-                        if (speaking && !paused) {
-                            pauseEle.className = 'paused';
-                            playEle.className = '';
-                            pause();
-                        }
-                    }
-
-                    function onClickStop() {
-                        if (speaking) {
-                            stopEle.className = 'stopped';
-                            playEle.className = pauseEle.className = '';
-                            flag = false;
-                            cancel();
-
-                        }
-                    }
-
-                }
-
-            else { /* speech synthesis not supported */
-                msg = document.createElement('h5');
-                msg.textContent = "Detected no support for Speech Synthesis";
-                msg.style.textAlign = 'center';
-                msg.style.backgroundColor = 'red';
-                msg.style.color = 'white';
-                msg.style.marginTop = msg.style.marginBottom = 0;
-                document.body.insertBefore(msg, document.querySelector('div'));
-            }
-
-        }
