b64afc737eb94d207bdf5f76e0a337a5d3e2777e
catchupTJC <53647636+catchupTJC@users.noreply.github.com>
2020-03-13 22:50:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,4 @@
 onload = function() {
-
-    let allVoices;
-    let speakerMenu;
-    let voiceIndex = 0;
-  
-    function init(){
-     speakerMenu = qs("#speakerMenu");
-     speakerMenu.addEventListener("change",selectSpeaker,false);    
-              
-    if ('speechSynthesis' in window) with(speechSynthesis) {        
-    if (speechSynthesis.onvoiceschanged !== undefined) {
-      //Chrome gets the voices asynchronously so this is needed
-      speechSynthesis.onvoiceschanged = setUpVoices;
-    }
-    setUpVoices(); //for all the other browsers
-    }else{
-    qs("#warning").style.display = "block";
-    } 
-    }
-  
-   function setUpVoices(){
-    allVoices = getAllVoices();
-    filterVoices();
-    }
-  
-   function createSpeakerMenu(voices){
-    let code = ``;
-    voices.forEach(function(vobj,i){
-      code += `<option value=${vobj.id}>${vobj.name} (${vobj.lang})`;
-      code += vobj.voiceURI.includes(".premium") ? ' (premium)' : ``;
-      code += `</option>`;
-    });
-    speakerMenu.innerHTML = code;
-    }
-
-    function selectSpeaker(){
-   voiceIndex = speakerMenu.selectedIndex;
-    }
-
-  function filterVoices(){
-  let langcode = "en";
-  voices = allVoices.filter(function (voice) {
-    return langcode === "all" ? true : voice.lang.indexOf(langcode + "-") >= 0;
-  });
-  createSpeakerMenu(voices);
-  speakerMenu.selectedIndex = voiceIndex;
-}
-  
-  function getAllVoices() {
-  let voicesall = speechSynthesis.getVoices();
-  let vuris = [];
-  let voices = [];
-  //unfortunately we have to check for duplicates
-  voicesall.forEach(function(obj,index){
-    let uri = obj.voiceURI;
-    if (!vuris.includes(uri)){
-        vuris.push(uri);
-        voices.push(obj);
-     }
-  });
-  voices.forEach(function(obj,index){obj.id = index;});
-  return voices;
-}
-
-function qs(selectorText){
-  //saves lots of typing for those who eschew Jquery
-  return document.querySelector(selectorText);
-}
-         
-
-document.addEventListener('DOMContentLoaded', function (e) {
-  try {init();} catch (error){
-    console.log("Data didn't load", error);}
-});
-    
-    //testing voice selection end (let sval in on click play need to edit also) 
     
         var playEle = document.querySelector('#play');
         var pauseEle = document.querySelector('#pause');
@@ -90,11 +14,11 @@ document.addEventListener('DOMContentLoaded', function (e) {
             if(!flag){
                 flag = true;
                 utterance = new SpeechSynthesisUtterance(document.querySelector('#readFrom').innerHTML);
-                utterance.rate = 0.8;
-                utterance.lang = 'en'; 
-                
+                utterance.rate = 0.7;            
+              
                 let sval = Number(speakerMenu.value);
                 utterance.voice = allVoices[sval];
+                utterance.lang = 'en'; 
               
                 utterance.onend = function(){
                     flag = false; playEle.className = pauseEle.className = ''; stopEle.className = 'stopped';
@@ -141,4 +65,5 @@ document.addEventListener('DOMContentLoaded', function (e) {
         document.body.insertBefore(msg, document.querySelector('div'));
     }
 
+}
 
