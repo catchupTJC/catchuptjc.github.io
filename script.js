6577045e471fd4877e4df2b2327cce0e93440302
catchupTJC <53647636+catchupTJC@users.noreply.github.com>
2020-03-14 01:55:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,76 @@
 onload = function() {
+
+  let allVoices;
+  let rateFld, speakerMenu;
+  let voiceIndex = 0;
+
+  function init(){
+  speakerMenu = qs("#speakerMenu");
+  speakerMenu.addEventListener("change",selectSpeaker,false);
+  rateFld = qs("#rateFld");
+  
+   if (window.speechSynthesis) {
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+      //Chrome gets the voices asynchronously so this is needed
+      speechSynthesis.onvoiceschanged = setUpVoices;
+    }
+    setUpVoices(); //for all the other browsers
+   }else{
+    speakerMenu.disabled = true;
+    qs("#warning").style.display = "block";
+   }
+  }
+   function setUpVoices(){
+   allVoices = getAllVoices();
+    filterVoices();
+   }
+
+  function createSpeakerMenu(voices){
+  let code = ``;
+  voices.forEach(function(vobj,i){
+    code += `<option value=${vobj.id}>${vobj.name} (${vobj.lang})`;
+    code += vobj.voiceURI.includes(".premium") ? ' (premium)' : ``;
+    code += `</option>`;
+  });
+  speakerMenu.innerHTML = code;
+}
+
+function selectSpeaker(){
+  voiceIndex = speakerMenu.selectedIndex;
+}
+
+function filterVoices(){
+  let langcode = "en";
+  voices = allVoices.filter(function (voice) {
+    return langcode === "all" ? true : voice.lang.indexOf(langcode + "-") >= 0;
+  });
+  createSpeakerMenu(voices);
+  speakerMenu.selectedIndex = voiceIndex;
+}
+
+function getAllVoices() {
+  let voicesall = speechSynthesis.getVoices();
+  let vuris = [];
+  let voices = [];
+  //unfortunately we have to check for duplicates
+  voicesall.forEach(function(obj,index){
+    let uri = obj.voiceURI;
+    if (!vuris.includes(uri)){
+        vuris.push(uri);
+        voices.push(obj);
+     }
+  });
+  voices.forEach(function(obj,index){obj.id = index;});
+  return voices;
+}
+function qs(selectorText){
+  return document.querySelector(selectorText);
+}
+document.addEventListener('DOMContentLoaded', function (e) {
+  try {init();} catch (error){
+    console.log("Data didn't load", error);}
+
+}); 
   
             if ('speechSynthesis' in window)
                 with (speechSynthesis) {
@@ -15,10 +87,13 @@ onload = function() {
                     function onClickPlay() {
                         if (!flag) {
                             flag = true;
-                            utterance = new SpeechSynthesisUtterance(document.querySelector('#readFrom').innerHTML);
+                            utterance = new SpeechSynthesisUtterance(document.querySelector('#readFrom').innerHTML);             
 
-                            utterance.lang = 'en';
-                            utterance.rate = 0.8;
+                            let sval = Number(speakerMenu.value);                            
+                            utterance.voice = allVoices[sval];
+                            utterance.lang = utterance.voice.lang;
+                            utterance.rate = Number(rateFld.value);
+
                             utterance.onend = function () {
                                 flag = false;
                                 playEle.className = pauseEle.className = '';
