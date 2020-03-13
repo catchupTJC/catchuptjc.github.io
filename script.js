138e62fd819b392dacdb56de61d043229f3d7ef2
catchupTJC <53647636+catchupTJC@users.noreply.github.com>
2020-03-13 16:48:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,4 @@
 onload = function() {
-  
-  //testing voice selection
-    if ('speechSynthesis' in window) with(speechSynthesis) {
-
-    let allVoices;
-    let speakerMenu;
-    let voiceIndex = 0;
-
-    speakerMenu = qs("#speakerMenu"); 
-    speakerMenu.addEventListener("change",selectSpeaker,false);
-
-    if (speechSynthesis.onvoiceschanged !== undefined) {
-      //Chrome gets the voices asynchronously so this is needed
-      speechSynthesis.onvoiceschanged = setUpVoices;
-    }
-    setUpVoices(); //for all the other browsers
-    }else{
-    qs("#warning").style.display = "block";
-    } 
-  
-   function setUpVoices(){
-    allVoices = getAllVoices();
-    filterVoices();
-    }
-  
-   function createSpeakerMenu(voices){
-    let code = ``;
-    voices.forEach(function(vobj,i){
-      code += `<option value=${vobj.id}>${vobj.name} (${vobj.lang})`;
-      code += vobj.voiceURI.includes(".premium") ? ' (premium)' : ``;
-      code += `</option>`;
-    });
-    speakerMenu.innerHTML = code;
-    }
-
-    function selectSpeaker(){
-   voiceIndex = speakerMenu.selectedIndex;
-    }
-
-  function filterVoices(){
-  let langcode = "en";
-  voices = allVoices.filter(function (voice) {
-    return langcode === "all" ? true : voice.lang.indexOf(langcode + "-") >= 0;
-  });
-  createSpeakerMenu(voices);
-  speakerMenu.selectedIndex = voiceIndex;
-}
-  
-  function getAllVoices() {
-  let voicesall = speechSynthesis.getVoices();
-  let vuris = [];
-  let voices = [];
-  //unfortunately we have to check for duplicates
-  voicesall.forEach(function(obj,index){
-    let uri = obj.voiceURI;
-    if (!vuris.includes(uri)){
-        vuris.push(uri);
-        voices.push(obj);
-     }
-  });
-  voices.forEach(function(obj,index){obj.id = index;});
-  return voices;
-}
-
-    //testing voice selection end
      
         var playEle = document.querySelector('#play');
         var pauseEle = document.querySelector('#pause');
@@ -79,11 +14,11 @@ onload = function() {
             if(!flag){
                 flag = true;
                 utterance = new SpeechSynthesisUtterance(document.querySelector('#readFrom').innerHTML);
-                utterance.rate = 0.7;            
+                utterance.rate = 0.8;
+                utterance.lang = 'en'; 
               
                 let sval = Number(speakerMenu.value);
-                utterance.voice = allVoices[sval];
-                utterance.lang = 'en'; 
+                utterance.voice = allVoices[sval];               
               
                 utterance.onend = function(){
                     flag = false; playEle.className = pauseEle.className = ''; stopEle.className = 'stopped';
