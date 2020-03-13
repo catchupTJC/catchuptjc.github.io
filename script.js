4e0308b5bf5fddc31303a7baa337fcf858287f30
catchupTJC <53647636+catchupTJC@users.noreply.github.com>
2020-03-13 23:05:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,91 @@
 onload = function() {
-    
-        var playEle = document.querySelector('#play');
-        var pauseEle = document.querySelector('#pause');
-        var stopEle = document.querySelector('#stop');
-        var flag = false;
 
-        playEle.addEventListener('click', onClickPlay);
-        pauseEle.addEventListener('click', onClickPause);
-        stopEle.addEventListener('click', onClickStop);
+let allVoices;
+let txtFld, rateFld, speakBtn, speakerMenu;
+let voiceIndex = 0;
 
-        function onClickPlay() {
-            if(!flag){
-                flag = true;
-                utterance = new SpeechSynthesisUtterance(document.querySelector('#readFrom').innerHTML);
-                utterance.rate = 0.7;            
-                utterance.lang = 'en-US'; 
-              
-                utterance.onend = function(){
-                    flag = false; playEle.className = pauseEle.className = ''; stopEle.className = 'stopped';
-                };
-                playEle.className = 'played';
-                stopEle.className = '';
-                speak(utterance);
-            }
-             if (paused) { /* unpause/resume narration */
-                playEle.className = 'played';
-                pauseEle.className = '';
-                resume();
-            } 
-        }
-
-        function onClickPause() {
-            if(speaking && !paused){ /* pause narration */
-                pauseEle.className = 'paused';
-                playEle.className = '';
-                pause();
-            }
-        }
-
-        function onClickStop() {
-            if(speaking){ /* stop narration */
-                /* for safari */
-                stopEle.className = 'stopped';
-                playEle.className = pauseEle.className = '';
-                flag = false;
-                cancel();
-
-            }
-        }
-
+function init(){
+  speakBtn = qs("#speakBtn");
+  txtFld = qs("#txtFld"); 
+  speakerMenu = qs("#speakerMenu");
+  speakBtn.addEventListener("click",talk,false); speakerMenu.addEventListener("change",selectSpeaker,false);
+  rateFld = qs("#rateFld");
+  
+   if (window.speechSynthesis) {
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+      //Chrome gets the voices asynchronously so this is needed
+      speechSynthesis.onvoiceschanged = setUpVoices;
     }
+    setUpVoices(); //for all the other browsers
+  }else{
+    speakBtn.disabled = true;
+    speakerMenu.disabled = true;
+    qs("#warning").style.display = "block";
+  }
+}
+function setUpVoices(){
+  allVoices = getAllVoices();
+  filterVoices();
+}
+function talk(){
+  let sval = Number(speakerMenu.value);
+  let u = new SpeechSynthesisUtterance();
+  u.voice = allVoices[sval];
+  u.lang = u.voice.lang;
+  u.text = txtFld.value;
+  u.rate = Number(rateFld.value);
+  speechSynthesis.speak(u);
+}
 
-    else { /* speech synthesis not supported */
-        msg = document.createElement('h5');
-        msg.textContent = "Detected no support for Speech Synthesis";
-        msg.style.textAlign = 'center';
-        msg.style.backgroundColor = 'red';
-        msg.style.color = 'white';
-        msg.style.marginTop = msg.style.marginBottom = 0;
-        document.body.insertBefore(msg, document.querySelector('div'));
-    }
+function createSpeakerMenu(voices){
+  let code = ``;
+  voices.forEach(function(vobj,i){
+    code += `<option value=${vobj.id}>${vobj.name} (${vobj.lang})`;
+    code += vobj.voiceURI.includes(".premium") ? ' (premium)' : ``;
+    code += `</option>`;
+  });
+  speakerMenu.innerHTML = code;
+}
+
+function selectSpeaker(){
+  voiceIndex = speakerMenu.selectedIndex;
+}
+
+function filterVoices(){
+  let langcode = "en";
+  voices = allVoices.filter(function (voice) {
+    return langcode === "all" ? true : voice.lang.indexOf(langcode + "-") >= 0;
+  });
+  createSpeakerMenu(voices);
+  speakerMenu.selectedIndex = voiceIndex;
+}
+
+function getAllVoices() {
+  let voicesall = speechSynthesis.getVoices();
+  let vuris = [];
+  let voices = [];
+  //unfortunately we have to check for duplicates
+  voicesall.forEach(function(obj,index){
+    let uri = obj.voiceURI;
+    if (!vuris.includes(uri)){
+        vuris.push(uri);
+        voices.push(obj);
+     }
+  });
+  voices.forEach(function(obj,index){obj.id = index;});
+  return voices;
+}
+
+// Generic Utility Functions
+function qs(selectorText){
+  //saves lots of typing for those who eschew Jquery
+  return document.querySelector(selectorText);
+}
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  try {init();} catch (error){
+    console.log("Data didn't load", error);}
+
+});
+
 }
