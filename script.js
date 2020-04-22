831ec0d6687283e12f7a694632195bb3b4b01ae6
catchupTJC <53647636+catchupTJC@users.noreply.github.com>
2020-04-22 11:48:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ onload = function() {
                     function onClickPlay() {
                         if (!flag) {
                             flag = true;
-                            utterance = new SpeechSynthesisUtterance(document.querySelector('body').textContent); 
+                            utterance = new SpeechSynthesisUtterance(document.querySelector('#readFrom').textContent); 
                      
                             let sval = Number(speakerMenu.value);                            
                             utterance.voice = allVoices[sval];
